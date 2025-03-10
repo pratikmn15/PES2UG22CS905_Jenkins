3252e2241e08485838fee331cfb3c696b42df96c
Pratik Naik <naik.pratik004@hotmail.com>
2025-03-10 16:08:05 +0530

    edit

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I PES2UG22CS905 have successfully built and run this  \n";
+  cout << "I PES2UG22CS905 have successfully built and run this \n";
+  cout<< "Edited\n";
   return 0;
 }
